8637bf24dc50fd84bd8c5f4080d6e13e07a24afb
Eric Krukowski <erickruk@amazon.com>
2023-09-04 01:39:42 -0400

    Lab complete

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,11 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        json_data = json.loads(self.get_response_body())
+        json_dump = json.dumps(json_data, indent=4, sort_keys=True)
+        return json_data
+
